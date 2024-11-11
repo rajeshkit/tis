6811be78d91518b446eeec614f29ba5491230b61
rajesh n <rajesh.kit@gmail.com>
2024-11-11 17:01:15 +0530

    welcome endpoint is added

--- src/main/java/com/tis/controller/TrainController.java
+++ src/main/java/com/tis/controller/TrainController.java
@@ -2,6 +2,7 @@ package com.tis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,10 @@ public class TrainController {
 	@RequestMapping("/hello")
 	public String hello1() {
 		return "This is from hello ";
+	}
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "welcome";
 	}
 	@RequestMapping(method = RequestMethod.POST,value = "/train")
 	public Train createTrain(@RequestBody Train train) {
